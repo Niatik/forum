7638fc90b41cca8f4d04f741046a1df3a6fc8b21
Nikita Dragunov <nikita.dragunov@gmail.com>
2018-09-06 11:25:58 +0600

    5-7 episodes.

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -25,11 +25,11 @@ class ParticipateInForumTest extends TestCase
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
         // Given we have the authenticated user
-        $this->be($user = factory('App\User')->create());
+        $this->be($user = create('App\User'));
         // And an existing thread
-        $thread = factory('App\Thread')->create();
+        $thread = create('App\Thread');
         // When the user adds a reply to the thread
-        $reply = factory('App\Reply')->make();
+        $reply = make('App\Reply');
         $this->post($thread->path().'/replies', $reply->toArray());
         // Then their reply should be visible on the page.
         $this->get($thread->path())
