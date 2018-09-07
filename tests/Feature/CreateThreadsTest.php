67282db214cc77a2d037f6a863c4af406df0e01f
Nikita Dragunov <nikita.dragunov@gmail.com>
2018-09-07 10:30:50 +0600

    8 episode.

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -15,9 +15,18 @@ class CreateThreadsTest extends TestCase
     function guest_may_not_create_threads()
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
+
         $thread = make('App\Thread');
 
         $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    function guests_cannot_see_the_create_thread_page()
+    {
+        $this->withExceptionHandling()
+            ->get('threads/create')
+            ->assertRedirect('/login');
     }
 
     /** @test */
