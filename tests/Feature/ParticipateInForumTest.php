089c585b13a899c5c940af4aa5ec5092718067fa
Nikita Dragunov <nikita.dragunov@gmail.com>
2018-09-12 11:03:58 +0600

    10 episode.

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -23,15 +23,26 @@ class ParticipateInForumTest extends TestCase
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
-        // Given we have the authenticated user
         $this->be($user = create('App\User'));
-        // And an existing thread
+
         $thread = create('App\Thread');
-        // When the user adds a reply to the thread
         $reply = make('App\Reply');
+
         $this->post($thread->path().'/replies', $reply->toArray());
-        // Then their reply should be visible on the page.
+
         $this->get($thread->path())
             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+
+        $thread = create('App\Thread');
+        $reply = make('App\Reply', ['body' => null]);
+
+        $this->post($thread->path().'/replies', $reply->toArray())
+            ->assertSessionHasErrors('body');
     }
 }
